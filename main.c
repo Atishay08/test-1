b461101050113cd6ec23106ea5b4342905b5c1bc
Atishay08 <87242244+Atishay08@users.noreply.github.com>
2024-03-21 23:19:52 +0530

    Update main.c

--- main.c
+++ main.c
@@ -14,5 +14,27 @@ int binary_search(int arr[], int left, int right, int key) {
 }
 
 int main() {
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
 
+    int arr[n]; // Variable-sized array declaration (supported in C99 and later)
+
+    printf("Enter %d integers in sorted order:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int key;
+    printf("Enter the element to search: ");
+    scanf("%d", &key);
+
+    int index = binary_search(arr, 0, n - 1, key);
+    if (index != -1) {
+        printf("Element found at index %d\n", index);
+    } else {
+        printf("Element not found\n");
+    }
+
+    return 0;
 }
